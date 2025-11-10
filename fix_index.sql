0b0fa18221fff6b0b55651f5e1816443a816f400
sajaadasyntax <nozanteam@gmail.com>
2025-11-10 15:36:02 +0200

    committed

--- fix_index.sql
+++ fix_index.sql
@@ -1,6 +1,8 @@
--- Fix for missing customers_isAgentCustomer_idx index
+-- Fix for missing indexes
 -- Run this on your main database before running migrations
 
--- Create the index if it doesn't exist
+-- Create the indexes if they don't exist
 CREATE INDEX IF NOT EXISTS "customers_isAgentCustomer_idx" ON "customers"("isAgentCustomer");
+CREATE INDEX IF NOT EXISTS "expenses_isDebt_idx" ON "expenses"("isDebt");
+CREATE INDEX IF NOT EXISTS "income_isDebt_idx" ON "income"("isDebt");
 
